0a6e22b57603c4607ce476c996647491bcacb819
RollsRoycewk <50765560@qq.com>
2020-12-30 12:14:15 +0800

    feat:nav列表样式以及动态数据渲染

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,25 +1,46 @@
 <template>
-	<!-- 头部 -->
-	<view class="header-container">
-		<image src="../../static/images/logo.png" class="header-image"></image>
-		
-		<view class="header-input-contaniner">
-			<view class="iconfont icon-sousuo"></view>
-			<input type="text" value="" class="header-search" placeholder="搜索商品" placeholder-class="placeholder"/>
+	<view class="">
+		<!-- 头部 -->
+		<view class="header-container">
+			<image src="../../static/images/logo.png" class="header-image"></image>
+			<view class="header-input-contaniner">
+				<view class="iconfont icon-sousuo"></view>
+				<input type="text" value="" class="header-search" placeholder="搜索商品" placeholder-class="placeholder"/>
+			</view>
+			<button  class="header-button">搜索</button>
 		</view>
-		<button  class="header-button">搜索</button>
+		<!-- navContainer -->
+		<scroll-view class="nav-container" scroll-x="">
+			<view class="nav-item active" >
+				推荐
+			</view>
+			<view class="nav-item" v-for="item in indexData.kingKongModule.kingKongList" :key="item.L1Id">
+				{{item.text}}
+			</view>
+		</scroll-view>
 	</view>
+
+	
 </template>
 
 <script>
 	export default {
 		data() {
 			return {
-				
+				indexData:{}
 			}
 		},
 		methods: {
 			
+		},
+		async mounted() {
+			const indexData = await uni.request({
+				url:"http://wangkai.zone:3579/index",
+				// success(val) {
+				// 	console.log(val)
+				// }
+			})
+			this.indexData = indexData[1].data
 		}
 	}
 </script>
@@ -27,7 +48,7 @@
 <style lang="stylus">
 	.header-container
 		display flex
-		margin 20upx 0
+		margin-top 20upx
 		align-items center
 	.header-image
 		width 140upx
@@ -48,4 +69,24 @@
 		margin 0 20rpx
 		flex-shrink 0
 		font-size 24upx
+	.nav-container
+		white-space nowrap
+		.nav-item
+			display inline-block
+			width 140upx
+			height 80upx
+			line-height 80upx
+			text-align center
+			font-size 28upx
+			position relative
+			&.active::after
+				display block
+				width 100%
+				height 2upx
+				background-color red
+				content ""
+				position absolute
+				left 0
+				bottom 2upx
+			
 </style>
--- unpackage/dist/dev/mp-weixin/pages/index/index.js
+++ unpackage/dist/dev/mp-weixin/pages/index/index.js
@@ -131,7 +131,19 @@ __webpack_require__.r(__webpack_exports__);
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0; //
+/* WEBPACK VAR INJECTION */(function(uni) {Object.defineProperty(exports, "__esModule", { value: true });exports.default = void 0;var _regenerator = _interopRequireDefault(__webpack_require__(/*! ./node_modules/@babel/runtime/regenerator */ 25));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };}function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) {try {var info = gen[key](arg);var value = info.value;} catch (error) {reject(error);return;}if (info.done) {resolve(value);} else {Promise.resolve(value).then(_next, _throw);}}function _asyncToGenerator(fn) {return function () {var self = this,args = arguments;return new Promise(function (resolve, reject) {var gen = fn.apply(self, args);function _next(value) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value);}function _throw(err) {asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err);}_next(undefined);});};} //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 //
 //
 //
@@ -147,11 +159,22 @@ Object.defineProperty(exports, "__esModule", { value: true });exports.default = 
 var _default =
 {
   data: function data() {
-    return {};
-
+    return {
+      indexData: {} };
 
   },
-  methods: {} };exports.default = _default;
+  methods: {},
+
+
+  mounted: function () {var _mounted = _asyncToGenerator( /*#__PURE__*/_regenerator.default.mark(function _callee() {var indexData;return _regenerator.default.wrap(function _callee$(_context) {while (1) {switch (_context.prev = _context.next) {case 0:_context.next = 2;return (
+                uni.request({
+                  url: "http://wangkai.zone:3579/index"
+                  // success(val) {
+                  // 	console.log(val)
+                  // }
+                }));case 2:indexData = _context.sent;
+              this.indexData = indexData[1].data;case 4:case "end":return _context.stop();}}}, _callee, this);}));function mounted() {return _mounted.apply(this, arguments);}return mounted;}() };exports.default = _default;
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["default"]))
 
 /***/ }),
 /* 21 */
